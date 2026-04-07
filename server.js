6acee1278778f7c72dd3d1a6e6ed8ad0fca9a8d3
Soumya Sankar Das <soumyadas429@gmail.com>
2026-04-07 22:12:05 +0530

    backend setup & db connection

---
+++ server.js
@@ -0,0 +1,11 @@
+import app from "./src/app.js";
+import dotenv from "dotenv";
+import { connectDB } from "./src/config/database.js";
+
+dotenv.config();
+
+await connectDB();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/models/user.model.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: [true, "Username already exists"] },
+    email: { type: String, required: true, unique: [true, "Email already exists"] },
+    password: { type: String, required: true },
+});
+
+const User = mongoose.model("UserModel", userSchema);
+export default User;
